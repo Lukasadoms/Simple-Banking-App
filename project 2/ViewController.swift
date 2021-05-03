bbc45f134e5c3312abeaa0cd9830c96433fc63b5
Lukas Adomavičius <73688674+Lukasadoms@users.noreply.github.com>
2021-05-03 18:10:20 +0300

    Initial Commit

---
+++ project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project 2
+//
+//  Created by Lukas Adomavicius on 5/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
